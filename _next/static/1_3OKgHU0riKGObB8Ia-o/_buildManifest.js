6b8242bdff5f952c4364cd561ce53e4bed843cec
caxone <alcaxone@gmail.com>
2023-04-17 12:13:29 +0000

    Deploying to gh-pages from @ caxone/my-page@1e8b043dbf912300a3c2e8b9a4dd3c66cca39372 🚀

---
+++ _next/static/1_3OKgHU0riKGObB8Ia-o/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/484-624add44c4a8a04d.js","static/chunks/pages/index-6001783e2bd4110f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/XfgqyIhVeBpJcWFrBVKVO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/484-683ac0a8e8b2659e.js","static/chunks/pages/index-d972922758d0c0f5.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
